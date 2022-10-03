b5ebf8a1256a9f4e5fa4cee723914aec4369152c
Anderson P. Brantes <andersonbrantes@gmail.com>
2022-10-02 23:20:39 -0300

    Adiciona estrutura inicial de paginas e inicialização do google maps

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import MapPage from './pages/MapPage';
+
+function App() {
+  return <MapPage />;
+}
+
+export default App;
---
+++ src/pages/MapPage.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import "./MapPage.css";
+
+const MapPage = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyBTEghxJTW6UNdkIdSWdn5pUOxoPmO9GSo"
+  })
+
+  return (
+    <div className="map">
+      {
+        isLoaded ? (
+          <GoogleMap
+            mapContainerStyle={{ width: "100%", height: "100%"}}
+            center={{
+              lat: -27.590824,
+              lng: -48.551262
+            }}
+            zoom={18}
+            onLoad={() => {}}
+            onUnmount={() => {}}
+          >
+            { /* Child components, such as markers, info windows, etc. */ }
+            <Marker position={{
+              lat: -27.590824,
+              lng: -48.551262
+            }} options={{
+              label: {
+                text: "Position test",
+                className: "map-marker"
+              }
+            }} />
+          </GoogleMap>
+      ) : <></>
+      }
+    </div>
+  );
+}
+
+export default MapPage;
+
