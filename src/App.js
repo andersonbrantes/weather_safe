7891c8368b846cc362207bd900a090f5e291c1ed
Anderson P. Brantes <andersonbrantes@gmail.com>
2022-10-09 13:05:02 -0300

    Adiciona componente para notificaoes de alertas de clima

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import logo from './logo.svg';
+import { MapContainer } from './components/map_container';
+import { NotificationContainer } from 'react-notifications';
+import FloatNotification from './components/float_notifications';
+
 import './App.css';
-import MapPage from './pages/MapPage';
+import 'react-notifications/lib/notifications.css';
 
 function App() {
-  return <MapPage />;
+  let weatherNotifications = [
+    { "type": "warning", "message": "Mensagem de teste" },
+    { "type": "error", "message": "Mensagem de teste" }
+  ]
+
+  // Teste para criar notificacoes
+  {
+    weatherNotifications.forEach((notification) => {
+      return FloatNotification.createNotification(notification.type);
+    })
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault(event);
+    console.log(event.target.name.value);
+    console.log(event.target.email.value);
+    console.log(event.target.latitude.value);
+    console.log(event.target.longitude.value);
+  };
+
+  return (
+    <div>
+      <NotificationContainer />
+            
+      <MapContainer onSubmit={onSubmit} />      
+    </div>
+  );
 }
 
 export default App;
