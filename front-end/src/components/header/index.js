38a40a880effb1eab5e23c10ef54b7f936a3d69c
Anderson P. Brantes <andersonbrantes@gmail.com>
2022-10-23 23:59:02 -0300

    Add application header component, fixed at the website top position

--- front-end/src/components/header/index.js
+++ front-end/src/components/header/index.js
@@ -1,11 +1,19 @@
-import { Component } from 'react';
-
-import '../../assets/stylesheets/header.css';
+import "./header.css";
+import UserAvatarImage from '../../assets/images/user-avatar.png';
 
 export const Header = () => {
   return (
     <header>
-      <h1>Weather Safe</h1>
+      <div class="dropdown float-left">
+        <div class="menu-button profile" aria-haspopup="true" aria-expanded="true">
+          <img src={UserAvatarImage} alt="UserAvatarImage" width="25" class="user-avatar" />
+        </div>
+
+        <ul class="dropdown-menu">
+          <li>Logout</li>
+        </ul>
+      </div>      
+      <h1 class="float-left">Weather Safe</h1>
     </header>
   );
 }
